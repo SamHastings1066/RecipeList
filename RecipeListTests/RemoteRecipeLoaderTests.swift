f27e75ed8c21c9b94337b7c4bc3b3a37be510032
Sam Hastings <samhastingsis@gmail.com>
2024-11-08 13:33:30 -0500

    Create HTTPClient protocol and HTTPClientSpy.

--- RecipeListTests/RemoteRecipeLoaderTests.swift
+++ RecipeListTests/RemoteRecipeLoaderTests.swift
@@ -8,20 +8,51 @@
 import XCTest
 
 class RemoteRecipeLoader {
+    let client: HTTPClient
+    let url: URL
     
+    init(client: HTTPClient, url: URL) {
+        self.client = client
+        self.url = url
+    }
+    
+    func load() {
+        client.get(from: url)
+    }
 }
 
-class HTTPClient {
+protocol HTTPClient {
+    func get(from url: URL)
+}
+
+class HTTPClientSpy: HTTPClient {
     var requestedURL: URL?
+    
+    func get(from url: URL) {
+        requestedURL = url
+    }
+    
 }
 
 final class RemoteRecipeLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClient()
-        let _ = RemoteRecipeLoader()
+        let url = URL(string: "http://a-given.com")!
+        let client = HTTPClientSpy()
+        let _ = RemoteRecipeLoader(client: client, url: url)
         
         XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_load_requestsDataFromURL() {
+        let url = URL(string: "http://a-given.com")!
+        let client = HTTPClientSpy()
+        let sut = RemoteRecipeLoader(client: client, url: url)
+        
+        
+        sut.load()
+        
+        XCTAssertEqual(url, client.requestedURL)
     }
 
 }
