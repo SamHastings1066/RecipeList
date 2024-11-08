1a96e27e20ee1bdc0c49e10cd8cabd72a7fb88e8
Sam Hastings <samhastingsis@gmail.com>
2024-11-08 12:31:37 -0500

    Initial Commit

---
+++ RecipeList/RecipeList.h
@@ -0,0 +1,18 @@
+//
+//  RecipeList.h
+//  RecipeList
+//
+//  Created by sam hastings on 08/11/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RecipeList.
+FOUNDATION_EXPORT double RecipeListVersionNumber;
+
+//! Project version string for RecipeList.
+FOUNDATION_EXPORT const unsigned char RecipeListVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RecipeList/PublicHeader.h>
+
+
