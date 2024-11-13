c771d9cccf15477414d3bc323f6f68f7ee371953
Sam Hastings <samhastingsis@gmail.com>
2024-11-13 18:16:32 -0500

    Give RecipeItem a public initializer so it can be used in the test target.

--- RecipeList/Recipe Feature/RecipeItem.swift
+++ RecipeList/Recipe Feature/RecipeItem.swift
@@ -15,4 +15,14 @@ public struct RecipeItem: Equatable {
     let uuid: String
     let source_url: String?
     let youtube_url: String?
+    
+    init(cuisine: String, name: String, photo_url_large: String?, photo_url_small: String?, uuid: String, source_url: String?, youtube_url: String?) {
+        self.cuisine = cuisine
+        self.name = name
+        self.photo_url_large = photo_url_large
+        self.photo_url_small = photo_url_small
+        self.uuid = uuid
+        self.source_url = source_url
+        self.youtube_url = youtube_url
+    }
 }
