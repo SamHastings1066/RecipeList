f98d7e80c064202c479e222cf1a46797fd13b399
Sam Hastings <samhastingsis@gmail.com>
2024-11-18 11:32:24 -0500

    Create iOS app and combine with macOS framework into a shared workspace.

---
+++ RecipeApp/RecipeApp/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  RecipeApp
+//
+//  Created by sam hastings on 18/11/2024.
+//
+
+import SwiftUI
+import RecipeList
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RecipeApp/RecipeApp/RecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAppApp.swift
+//  RecipeApp
+//
+//  Created by sam hastings on 18/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
