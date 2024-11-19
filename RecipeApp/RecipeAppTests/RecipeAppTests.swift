a67ac8a2953de2fbc29bccd15cf50276ae767ec3
Sam Hastings <samhastingsis@gmail.com>
2024-11-19 12:25:29 -0500

    Add RecipeAppTests

---
+++ RecipeApp/RecipeAppTests/RecipeAppTests.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeAppTests.swift
+//  RecipeAppTests
+//
+//  Created by sam hastings on 19/11/2024.
+//
+
+import XCTest
+
+final class RecipeAppTests: XCTestCase {
+
+
+    func test_zero() throws {
+        //XCTFail()
+    }
+
+
+}
